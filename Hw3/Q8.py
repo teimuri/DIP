54a8ae65d485db1fea063f2bc2a14c509a470d2e
teimuri <teimurijervakani@gmail.com>
2023-05-07 14:49:05 +0330

    starting

--- Hw3/Q8.py
+++ Hw3/Q8.py
@@ -3,6 +3,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+#show list of available folders in directory
+import os
+os.listdir()
+
 
 #read the image
 #load the image from images folder
